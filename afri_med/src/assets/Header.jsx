2fc5f477c3c10cb99ba783010ebff55cd1dd79a0
username <barasamica@gmail.com>
2024-03-28 17:22:29 +0300

    Styled the app

--- afri_med/src/assets/Header.jsx
+++ afri_med/src/assets/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-const Header = () => {
+import
+{BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify} 
+from 'react-icons'
+
+function Header () {
   return (
-    <div>
-      
+   <header className='header'>
+    <div className='menu-icon'>
+        <BsJustify className='icon' />
     </div>
+    <div className='header-left'>
+        <BsSearch className='icon' />
+    </div>
+    <div className='header-right'>
+        <BsFillBellFill className='icon'/>
+        <BsFillEnvelopeFill className='icon'/>
+        <BsPersonCircle className='icon'/>
+    </div>
+   </header>
   )
 }
 
